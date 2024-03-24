315fdbf5519d3eeb645b7776b599b4b6117adaa6
prajjwaljoshi <88524726+prajjwaljoshi@users.noreply.github.com>
2024-03-24 23:09:55 +0530

    FeaturedItemsCarousel.js

--- my/src/FeaturedItemsCarousel.js
+++ my/src/FeaturedItemsCarousel.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function FeaturedItemsCarousel({ items }) {
   return (
     <Carousel
-      infiniteLoop // Enable continuous looping
-      autoPlay // Set autoplay with an optional delay
-      showThumbs={false} // Hide item thumbnails (optional)
-      showArrows={true} // Show navigation arrows
+      infiniteLoop
+      autoPlay
+      showThumbs={false}
+      showArrows={true}
     >
       {items.map((item) => (
         <div key={item.id} className="carousel-item">
-          {/* Content for each featured item */}
           <img src={item.imageUrl} alt={item.name} />
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          {/* Add a button or link if needed */}
+          <div className="item-details">
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+          </div>
         </div>
       ))}
     </Carousel>
